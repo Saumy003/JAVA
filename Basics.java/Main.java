0114e9e8af2907222a2854278486105363349718
Saumy003 <saumysingh2005@gmail.com>
2026-04-02 19:07:41 +0530

    First code in JAVA

---
+++ Basics.java/Main.java
@@ -0,0 +1,7 @@
+void main (){
+    System.out.println("Jai Rajputana" + " " + "Jai Bhawani");
+    System.out.println(3);
+    System.out.println(4 + 3);
+    System.out.println("4 + 3");
+    // This is my first JAVA code //
+}
