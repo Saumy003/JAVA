b5f8ef87e0cc11d4250ba8381a3fe7b999e4017f
Saumy003 <saumysingh2005@gmail.com>
2026-07-30 19:07:11 +0530

    Variables in JAVA

---
+++ 2_Variables/Variable.java
@@ -0,0 +1,14 @@
+public class Variable {
+    
+   public static void main() {
+   int age = 21;
+   System.out.println(age);
+   
+   String name = "Alice";
+   System.out.println(name);
+   
+   double salary = 45000.59;
+   System.out.println(salary);
+
+   } 
+}
