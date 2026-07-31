c1bc56e32d6960c253cb9ea2bf0bb302f30da88b
Saumy003 <saumysingh2005@gmail.com>
2026-08-01 01:07:01 +0530

    Arithmatic Operators in JAVA

---
+++ 4_Operators/Arithmetic.java
@@ -0,0 +1,21 @@
+public class Arithmetic {
+
+    public static void main(String[] args) {
+        
+        int a = 25;
+        int b = 15;
+
+        int total = a + b;
+        int difference = a - b;
+        int projected = a * 4;
+        int average = a / 7;
+        int remainder = a % 7;
+
+        System.out.println(total);
+        System.out.println(difference);
+        System.out.println(projected);
+        System.out.println(average);
+        System.out.println(remainder);
+    }
+    
+}
