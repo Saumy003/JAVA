2f604e56527fe5ab18167ff6d1408905b0bf9395
Saumy003 <saumysingh2005@gmail.com>
2026-08-03 00:14:48 +0530

    Unary Inc./Dec. Operators

---
+++ 4_Operators/Unary.java
@@ -0,0 +1,17 @@
+public class Unary {
+    
+    public static void main(String[] args) {
+        
+        int a = 10;
+        System.out.println(++a);
+
+        int d = 10;
+        System.out.println(d--);
+
+        int b = 5;
+        System.out.println(--b);
+
+        int c = 5;
+        System.out.println(c--);
+    }
+}
