c871df009f9f75450c29f5e60f208d767399678c
Saumy003 <saumysingh2005@gmail.com>
2026-08-04 17:47:55 +0530

    Some Problems using Bitwise Operators

--- 4_Operators/BitwiseProblems.java
+++ 4_Operators/BitwiseProblems.java
@@ -2,7 +2,24 @@ public class BitwiseProblems {
 
     public static void main(String[] args) {
         
-        
+        // check even & odd
+        int a = 5;
+
+        if ((a & 1) == 0) {
+            System.out.println("Even");
+        }
+        else{
+            System.out.println("Odd");
+        }
+
+        // check power of two
+        int n = 512;
+        if ((n & n-1) == 0) {
+            System.out.println("Power of 2");
+        }
+        else{
+            System.out.println("Not a power of 2");
+        }
+
     }
-    
 }
