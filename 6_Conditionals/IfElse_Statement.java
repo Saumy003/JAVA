9dd72847a1f008a8c50fc512a31cff8a962c4c9d
Saumy003 <saumysingh2005@gmail.com>
2026-08-09 19:17:27 +0530

    2. If Else Statement in Java

--- 6_Conditionals/IfElse_Statement.java
+++ 6_Conditionals/IfElse_Statement.java
@@ -12,5 +12,15 @@ public class IfElse_Statement {
             System.out.println("Fail");
         }
 
+        // Example 2
+
+        int age = 12;
+
+        if (age >= 18) {
+            System.out.println("Can Vote!");
+        }
+        else {
+            System.out.println("Cannot Vote!");
+        }
     }
 }
