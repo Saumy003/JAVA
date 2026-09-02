13e73b3f294125a49e56e9640bb5e447fd7a546b
Saumy003 <saumysingh2005@gmail.com>
2026-09-02 23:20:40 +0530

    Pattern 2. Solid Square

--- 8_PatternPrinting/Pattern_Two.java
+++ 8_PatternPrinting/Pattern_Two.java
@@ -1,6 +1,11 @@
 public class Pattern_Two {
     
     public static void main(String[] args) {
-        
+        for(int i = 1; i <= 5; i++) {
+            for(int j = 1; j <= 5; j++) {
+                System.out.print("*" + " ");
+            }
+            System.out.println();
+        }
     }
 }
