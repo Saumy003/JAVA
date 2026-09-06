3d91730e870c80df5e1856871fd1b9abe55f50e5
Saumy003 <saumysingh2005@gmail.com>
2026-09-07 00:14:20 +0530

    Method and Variable Scoping

--- 9_Methods/VariableScoping.java
+++ 9_Methods/VariableScoping.java
@@ -1,3 +1,8 @@
 public class VariableScoping {
-    
+
+    // Static or Class Variable Scope
+    static String college = "ABC College";
+    public static void main(String[] args) {
+        System.out.println(college);
+    }
 }
