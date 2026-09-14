2a2c86d3a4ffd1122808292937809b1dc5dc2bc0
Saumy003 <saumysingh2005@gmail.com>
2026-09-14 10:49:59 +0530

    2. Finally Block

---
+++ 13_Exception/Finally.java
@@ -0,0 +1,14 @@
+public class Finally {
+    
+    public static void main(String[] args) {
+        
+        try {
+            int result = 10 / 2;
+            System.out.println(result);
+        }catch (ArithmeticException e) {
+            System.out.println("Error occurred");
+        }finally {
+            System.out.println("Finally block executed");
+        }
+    }
+}
