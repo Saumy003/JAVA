49f53a1738540350ea95c2a141944a976f730164
Saumy003 <saumysingh2005@gmail.com>
2026-09-14 10:52:27 +0530

    3. Throw Keyword

---
+++ 13_Exception/Throw.java
@@ -0,0 +1,13 @@
+public class Throw {
+
+    public static void main(String[] args) {
+
+        int age = 15;
+
+        if (age < 18) {
+            throw new ArithmeticException("Age must be 18 or above");
+        }
+
+        System.out.println("Eligible");
+    }
+}
