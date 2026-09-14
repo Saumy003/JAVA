10e4d10d430bbf9aad4058ff9644cc68daa5a696
Saumy003 <saumysingh2005@gmail.com>
2026-09-14 11:02:42 +0530

    4. Throws Keyword

---
+++ 13_Exception/ThrowsKeyword.java
@@ -0,0 +1,19 @@
+public class ThrowsKeyword {
+    
+    public static void checkAge(int age) throws Exception {
+
+    if (age < 18) {
+        throw new Exception("Not eligible");
+    }
+
+    System.out.println("Eligible");
+    }
+
+    public static void main(String[] args) {
+        try {
+            checkAge(12);
+        } catch(Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
